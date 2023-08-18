d06405379a545222b78b47bb42399c7823439218
root <root@labdevweb.subnet08121225.vcn08121225.oraclevcn.com>
2023-08-18 01:48:02 +0000

    fix conflicts

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -99,7 +99,6 @@ class FuncionarioController extends Controller
         return response()->json($message);
     }
 
-<<<<<<< HEAD
     public function delete($id){  
         $result = $this->model->find($id);
 
@@ -112,15 +111,5 @@ class FuncionarioController extends Controller
         $message = ["message" => "Funcionário deletado com sucesso"];
         
         return response()->json($message);
-=======
-    public function delete($id, Request $request){
-        
-        $func = $this->model->find($id);
-        
-        ->delete();
-
-        return response()->json(null);
->>>>>>> c674684df3e0c625b42a9c10e7e08cfc8a1663b5
-    }
+   }
 }
-
